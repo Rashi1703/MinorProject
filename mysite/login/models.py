780548678e549204c7e48f74cedb36352850ed1e
Rashi1703 <Rashijain1710@gmail.com>
2024-01-10 17:33:03 +0530

    Added

--- mysite/login/models.py
+++ mysite/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=6, primary_key=True)
+    password = models.IntegerField(max_length=4, null=False)
+    mail = models.EmailField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.username
--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Users
 # Create your views here.
 def index(request):
     return render(request, 'login.html')
+
+def success(request):
+    if request.method == 'POST':
+        entered_username = request.POST.get('username')
+        entered_password = request.POST.get('password')
+        Users_registered=Users.objects.filter(username=entered_username,password=entered_password)
+        if Users_registered.exists():
+            return HttpResponse('success')
+        else:
+            return redirect("index")
+    return redirect("index")
+
